328df210bdb9965596315caa339783feda8704be
kingyj00 <onshopyj@gmail.com>
2025-02-14 15:41:47 +0900

    명언:1단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,16 @@
 package com.ll
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("===[ 명언앱 ]===")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    while (true) {
+        print("명언 : ")
+
+        val input = readlnOrNull()
+
+        if (input== "종료"){
+            break
+        }
+        println("입련한 명언 : $input")
     }
 }
