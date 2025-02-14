ffd0f03d4650155403eaebe16f8394a52a1b8feb
kingyj00 <onshopyj@gmail.com>
2025-02-14 16:05:17 +0900

    명언:2단계

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,28 @@
 package com.ll
 
 fun main() {
-    println("===[ 명언앱 ]===")
+    println("===[ 명언 앱 ]===")
+
+    val quotes = mutableListOf<Pair<String, String>>()
 
     while (true) {
-        print("명언 : ")
+        print("명령) ")
+        val command = readlnOrNull()?.trim()
 
-        val input = readlnOrNull()
-
-        if (input== "종료"){
-            break
+        when (command) {
+            "등록" -> {
+                print("명언 : ")
+                val quote = readlnOrNull() ?: ""
+                print("작가 : ")
+                val author = readlnOrNull() ?: ""
+                quotes.add(Pair(quote, author))
+                println("명언이 등록되었습니다!")
+            }
+            "종료" -> {
+                println("프로그램을 종료합니다.")
+                break
+            }
+            else -> println("알 수 없는 명령어입니다.")
         }
-        println("입련한 명언 : $input")
     }
 }
